703e88e0f98684edbe7cf48bc6ee0570b7c83846
natehimself <52184382+natehimself@users.noreply.github.com>
2022-01-17 17:18:51 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,11 +1,15 @@
 import random
+import pytest  
+import yaml
+import json
+
+
 
 def xToY():
-    x = 4
-    y = 3
-    if x >= y:
-        print("Please make sure X is the low number.")
-        quit()
+    with open('config.yml', 'r') as file:
+        import_service = yaml.safe_load(file)
+        x = import_service['config_settings']['x']
+        y = import_service['config_settings']['y']
     return x,y
 
 def randomCreation(x,y):
@@ -14,7 +18,7 @@ def randomCreation(x,y):
 
 def numberinput():
     while True:
-        number = input("Pick a number {} to {}: ".format(str(x),str(y)))
+        number = input(f"Pick a number {str(x)} to {str(y)}: ")
         try:
             val = int(number)
             if val < 1:
@@ -26,8 +30,10 @@ def numberinput():
     return number
 
 def guessing(randomnumber):
+    guessList = []
     while True:
         guess = int(numberinput())
+        guessList.append(guess)
         print("You guessed: ", guess)
         if guess > randomnumber: 
             print("Your guess was too high.")
@@ -35,11 +41,48 @@ def guessing(randomnumber):
             print("Your guess was too low.")
         elif guess == randomnumber:
                 break
-    return guess
+    return guess, guessList
 
 def answer(guess):
     print("Your guess of",guess,"was correct. Good Job!")
 
+def test_one():
+    x,y = xToY()
+    assert x < y
+
+def logging(randomnumber, guess, guessList):
+    logginglist = {'Guess': guess,
+    'Guesses': len(guessList),
+    'GuessList': guessList
+    }
+    return logginglist
+
+def newfile(filename='log.json'):
+    newloginit = {
+            "Guessing": [
+   
+                ]
+            }
+    with open(filename, 'r') as new_file:
+        data = new_file.read()
+        numchar = len(data)
+    if numchar < 1:
+        with open(filename, 'w') as new_file:
+            json.dump(newloginit, new_file, indent = 3)
+
+def logwriter(logginglist, filename='log.json'):
+    with open(filename, 'r') as json_file:
+        json_data = json.load(json_file)
+    with open(filename, 'w') as json_file:
+        json_data["Guessing"].append(logginglist)
+        json.dump(json_data, json_file, indent=3)
+
+
 if __name__ == '__main__':
     x,y = xToY()
-    answer(guessing(randomCreation(x,y)))
+    randomnumber = randomCreation(x, y)
+    guess, guessList = guessing(randomCreation(x,y))
+    answer(guess)
+    newfile()
+    logginglist = logging(randomnumber, guess, guessList)
+    logwriter(logginglist)
